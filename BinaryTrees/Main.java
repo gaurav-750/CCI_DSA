82e4705efd44917af1d918cdc01f8a24a0e36cc2
Gaurav <gauravsomani52750@gmail.com>
2023-04-22 23:41:04 +0530

    binary trees basic questions

--- BinaryTrees/Main.java
+++ BinaryTrees/Main.java
@@ -23,10 +23,39 @@ public class Main {
 //        postOrder(root);
 
 //        todo Level Order Traversal
-        levelOrder(root);
+//        levelOrder(root);
 
+//        todo Count number of nodes in Binary Tree
+//        System.out.println("Number of nodes: " + countNodes(root));
 
+//        todo Sum of nodes in Binary Tree
+//        System.out.println("Sum of nodes: " + sumOfNodes(root));
 
+//        todo Height of Binary Tree
+        System.out.println("Height: " + heightOfBTree(root));
+
+    }
+
+    private static int heightOfBTree(BinaryTreeNode<Integer> root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null)
+            return 1;
+
+        return Math.max(heightOfBTree(root.left), heightOfBTree(root.right)) + 1;
+    }
+
+    private static int sumOfNodes(BinaryTreeNode<Integer> root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null)
+            return root.data;
+
+         return root.data + sumOfNodes(root.left) + sumOfNodes(root.right);
+    }
+
+    private static int countNodes(BinaryTreeNode<Integer> root) {
+        if (root == null) return 0;
+
+        return countNodes(root.left) + countNodes(root.right) + 1;
     }
 
     private static void levelOrder(BinaryTreeNode<Integer> root) {
