9a9fa59ea0c78b1754b8586aab7942b67f3e6d0d
Gaurav <gauravsomani52750@gmail.com>
2023-05-01 11:38:58 +0530

    binary search medium problems

--- BinarySearch/Neetcode.java
+++ BinarySearch/Neetcode.java
@@ -1,11 +1,14 @@
 package BinarySearch;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Neetcode {
     public static void main(String[] args) {
 
-        int[] arr = {-4,-1,0,3,10};
+        int[] arr = {5,7,7,8,8,10};
 
 //        todo https://leetcode.com/problems/binary-search/
 //        search(arr, 9);
@@ -27,11 +30,148 @@ public class Neetcode {
 //        System.out.println("Is Present in Matrix: " + searchIn2D(arr2D, 0, 164));
 
 //        todo https://leetcode.com/problems/koko-eating-bananas/
-        int[] piles = {805306368,805306368,805306368};
-        System.out.println("Min Eating Speed: " + minEatingSpeed(piles, 1000000000));
+//        int[] piles = {805306368,805306368,805306368};
+//        System.out.println("Min Eating Speed: " + minEatingSpeed(piles, 1000000000));
 
+//        todo https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+//        int[] arr = {3,4,5,6,7,0,1,2};
+//        findMin(arr);
 
+//        todo https://leetcode.com/problems/search-in-rotated-sorted-array/
+//        System.out.println("Search in rotated array: " + searchInRotatedArray(arr, 0));
 
+//        todo https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+//        searchRange(arr, 8);
+
+//        todo https://leetcode.com/problems/search-suggestions-system/
+        String[] products = {"mobile","mouse","moneypot","monitor","mousepad"};
+        String searchWord = "mouse";
+        System.out.println(searchSuggestion(products, searchWord));
+
+    }
+
+    private static List<List<String>> searchSuggestion(String[] products, String searchWord) {
+//        sort the products
+        Arrays.sort(products);
+        int s = 0, e = products.length-1;
+        List<List<String>> res = new ArrayList<>();
+
+        for (int i = 0; i < searchWord.length(); i++) {
+            char c = searchWord.charAt(i); //current character
+            while (s <= e && (products[s].length() <= i || products[s].charAt(i) != c))
+                s++;
+            while (s <= e && (products[s].length() <= i || products[e].charAt(i) !=c))
+                e--;
+
+            int remain = e-s+1;
+            ArrayList<String> innerList = new ArrayList<>();
+            for (int j = 0; j < Math.min(3, remain); j++) {
+                innerList.add(products[s+j]);
+            }
+            res.add(innerList);
+        }
+        return res;
+    }
+
+    private static int[] searchRange(int[] arr, int x) {
+        int[] res = new int[2];
+        res[0] = firstPosition(arr, x);
+        res[1] = lastPosition(arr, x);
+        return res;
+    }
+
+    private static int firstPosition(int[] arr, int x) {
+        int s = 0, e = arr.length-1;
+        boolean isPresent = false;
+
+        while (s <= e){
+            int mid = (s+e)/2;
+            if (arr[mid] == x){
+                e = mid-1;
+                isPresent = true;
+            }else if(arr[mid] > x)
+                e = mid-1;
+            else
+                s = mid+1;
+        }
+
+        System.out.println(e+1);
+        return isPresent ? e+1 : -1;
+    }
+
+    private static int lastPosition(int[] arr, int x) {
+        int s = 0, e = arr.length-1;
+        boolean isPresent = false;
+
+        while (s <= e){
+            int mid = (s+e)/2;
+            if (arr[mid] == x){
+                s = mid+1;
+                isPresent = true;
+            }else if (arr[mid] > x)
+                e = mid-1;
+            else
+                s = mid+1;
+        }
+        return isPresent ? s-1 : -1;
+    }
+
+    private static int searchInRotatedArray(int[] arr, int k) {
+        //get the pivot Index
+        int pivot = getPivotIndex(arr);
+        int ans = binarySearch(arr, k, 0, pivot-1);
+        if (ans != -1) return ans;
+
+        return binarySearch(arr, k, pivot, arr.length-1);
+    }
+
+    private static int binarySearch(int[] arr, int k, int s, int e) {
+        while (s <= e){
+            int mid = (s+e)/2;
+            if (arr[mid] == k)
+                return mid;
+            else if (arr[mid] > k)
+                e = mid-1;
+            else
+                s = mid+1;
+        }
+        return -1;
+    }
+
+    private static int getPivotIndex(int[] arr) {
+        int s = 0, e = arr.length-1;
+        int pivotIndex = 0;
+
+        while (s <= e){
+            int mid = (s+e)/2;
+            if (mid != arr.length-1 && arr[mid] > arr[mid+1])
+                return mid+1;
+
+            if (arr[s] <= arr[mid])
+                s = mid+1;
+            else
+                e = mid-1;
+        }
+        return pivotIndex;
+    }
+
+    private static int findMin(int[] arr) {
+        int s = 0, e = arr.length-1;
+        int res = arr[0];
+
+        while (s <= e){
+            if (arr[s] <= arr[e])
+                return Math.min(res, arr[s]);
+
+            int mid = (s+e)/2;
+            res = Math.min(res, arr[mid]);
+
+            if (arr[s] <= arr[mid])
+                s = mid+1;
+            else //arr[mid] < arr[s]
+                e = mid-1;
+        }
+        return res;
     }
 
     private static int minEatingSpeed(int[] piles, int h) {
