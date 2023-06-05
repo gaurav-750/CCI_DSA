250db221b3ac1853d6df0bf02c6e1e6cc1337cd3
Gaurav <gauravsomani52750@gmail.com>
2023-06-05 23:23:34 +0530

    dp strings Qsd

--- Dynamic_Programming/Dp_Strings.java
+++ Dynamic_Programming/Dp_Strings.java
@@ -1,15 +1,49 @@
 package Dynamic_Programming;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Dp_Strings {
     public static void main(String[] args) {
 //        DP on Strings
 //        todo https://leetcode.com/problems/longest-common-subsequence/
-        System.out.println("LCS: " + longestCommonSubsequence("adebc", "acadb"));
+//        System.out.println("LCS: " + longestCommonSubsequence("adebc", "acadb"));
+
+//        todo https://www.geeksforgeeks.org/printing-longest-common-subsequence/
+//        System.out.println("Longest Common Subsequence: " +
+//                printLCS(0, 0, "adebc", "acadb"));
+
+//        todo https://leetcode.com/problems/longest-palindromic-subsequence/
+        System.out.println("Longest Palindromic Subsequence: " +
+                longestPalindromicSubsequence("bbbab"));
 
 
+    }
 
+    private static int longestPalindromicSubsequence(String s1) {
+        String s2 = "";
+        for (int i = s1.length()-1; i >= 0; i--) {
+            s2 += s1.charAt(i);
+        }
+
+        return longestCommonSubsequence(s1, s2);
+    }
+
+    private static String printLCS(int i, int j, String s1, String s2) {
+//        base case
+        if (i >= s1.length() || j >= s2.length())
+            return "";
+
+        //match
+        if (s1.charAt(i) == s2.charAt(j)){
+            return s1.charAt(i) + printLCS(i+1, j+1, s1, s2);
+        }
+
+        //not match
+        String ans1 = printLCS(i+1, j, s1, s2);
+        String ans2 = printLCS(i, j+1, s1, s2);
+
+        return ans1.length() > ans2.length() ? ans1 : ans2;
     }
 
     private static int longestCommonSubsequence(String s1, String s2) {
