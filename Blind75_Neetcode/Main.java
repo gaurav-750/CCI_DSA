667786db2407dc0feb9487379b16ff746dc7e43d
Gaurav <gauravsomani52750@gmail.com>
2023-07-29 00:09:29 +0530

    blind 75 questions on linked list and trees

--- Blind75_Neetcode/Main.java
+++ Blind75_Neetcode/Main.java
@@ -2,6 +2,11 @@ package Blind75_Neetcode;
 
 import java.util.*;
 
+import BinaryTrees.BinaryTree;
+import BinaryTrees.TreeNode;
+import LinkedList.Linked_List;
+import LinkedList.Node;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,7 +14,7 @@ public class Main {
         // 19:00
 //        TODO  1.ARRAYS AND HASHING
 //        todo  https://leetcode.com/problems/contains-duplicate/
-        int[] nums = {-1,0,1,2,-1,-4};
+//        int[] nums = {4,5,6,7,0,1,2};
 //        containsDuplicate(nums);
 
 //        todo https://leetcode.com/problems/valid-anagram/
@@ -57,9 +62,192 @@ public class Main {
 //        lengthOfLongestSubstring("pwwkew");
 
 //        todo https://leetcode.com/problems/longest-repeating-character-replacement/
-        characterReplacement("AABABBA", 1);
+//        characterReplacement("AABABBA", 1);
 
 
+        // Date: 28-07-2023
+//        todo https://leetcode.com/problems/valid-parentheses/
+//        System.out.println("Is Valid: " + isValid("({}[])"));
+
+//        todo https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+//        int[] nums = {1};
+//        System.out.println("Min Element: " + findMin(nums));
+
+//        todo https://leetcode.com/problems/reorder-list/
+//        Linked_List ll = new Linked_List();
+//        Node<Integer> head = ll.takeInput();
+//        reorderList(head);
+
+//        todo https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+//        ll.printLL(removeNthFromEnd(head, 1));
+
+//        todo https://leetcode.com/problems/invert-binary-tree/
+        BinaryTree obj = new BinaryTree();
+        TreeNode<Integer> root = obj.takeInput();
+//        obj.printBinaryTree(invertBinaryTree(root));
+
+//        todo https://leetcode.com/problems/same-tree/
+        TreeNode<Integer> root2 = obj.takeInput();
+//        System.out.println("Is same tree: " + isSameTree(root, root2));
+
+//        todo https://leetcode.com/problems/subtree-of-another-tree/
+        System.out.println("Is Sub Root: " + isSubTree(root, root2));
+
+
+
+    }
+
+    private static boolean isSubTree(TreeNode<Integer> root, TreeNode<Integer> subRoot) {
+        //base case
+        if (root == null)
+            return false;
+
+        if (root.data == subRoot.data){
+            if (isSameTree(root, subRoot))
+                return true;
+        }
+
+        return isSubTree(root.left, subRoot) || isSubTree(root.right, subRoot);
+    }
+
+    private static boolean isSameTree(TreeNode<Integer> root1, TreeNode<Integer> root2) {
+        //base case
+        if (root1 == null && root2 == null)
+            return true;
+        if (root1 == null || root2 == null || root1.data != root2.data)
+            return false;
+
+        return isSameTree(root1.left, root2.left) &&
+                isSameTree(root1.right, root2.right);
+    }
+
+    private static TreeNode<Integer> invertBinaryTree(TreeNode<Integer> root) {
+        //base case
+        if (root == null)
+            return null;
+        if (root.left == null && root.right == null)
+            return root;
+
+        TreeNode<Integer> leftNode = invertBinaryTree(root.left);
+        root.left = invertBinaryTree(root.right);
+        root.right = leftNode;
+        return root;
+    }
+
+    private static Node<Integer> removeNthFromEnd(Node<Integer> head, int k) {
+        if (head == null) return null;
+
+        Node<Integer> slow = head, fast = head;
+        for (int i = 0; i < k; i++)
+            fast = fast.next;
+
+        if (fast == null)
+            return slow.next;
+
+        while (fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+
+    private static void reorderList(Node<Integer> head) {
+        if (head == null || head.next == null)
+            return;
+
+        //get the middle of the linked list
+        Node<Integer> middle = getMiddle(head);
+
+        //reverse the linked list from the middle
+        Node<Integer> rHead = reverseLL(middle);
+//        System.out.println(middle.data + ", " + rHead.data);
+
+        Node<Integer> temp = new Node<>(0);
+        while (true){
+            temp.next = head;
+            head = head.next;
+            temp = temp.next;
+
+            temp.next = rHead;
+            rHead = rHead.next;
+            temp = temp.next;
+
+            if (head == rHead || head == temp){
+                break;
+            }
+        }
+    }
+
+    private static Node<Integer> reverseLL(Node<Integer> head) {
+        Node<Integer> past = null, present = head, future = head.next;
+
+        while (present != null){
+            present.next = past;
+            past = present;
+            present = future;
+
+            if (future != null)
+                future = future.next;
+        }
+        return past;
+    }
+
+    private static Node<Integer> getMiddle(Node<Integer> head) {
+        Node<Integer> slow = head, fast = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private static int findMin(int[] nums) {
+        int l = 0, r = nums.length-1;
+        int res = nums[0];
+
+        while (l <= r){
+            if (nums[l] < nums[r]){
+                res = Math.min(res, nums[l]);
+                break;
+            }
+
+            int mid = (l+r)/2;
+            res = Math.min(res, nums[mid]);
+
+            if (nums[mid] >= nums[l])
+                l = mid+1;
+            else
+                r = mid-1;
+        }
+        return res;
+    }
+
+    private static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i); //current bracket
+
+            if (c == '(' || c == '{' || c == '[')
+                stack.push(c);
+            else if (c == ')'){
+                if (!stack.isEmpty() && stack.peek() == '(')
+                    stack.pop();
+                else return false;
+            }else if (c == '}'){
+                if (!stack.isEmpty() && stack.peek() == '{')
+                    stack.pop();
+                else return false;
+            }else if (c == ']'){
+                if (!stack.isEmpty() && stack.peek() == '[')
+                    stack.pop();
+                else return false;
+            }
+        }
+
+        return stack.isEmpty();
     }
 
     private static int characterReplacement(String s, int k) {
